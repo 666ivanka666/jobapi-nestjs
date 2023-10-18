eaf22eafcbbb68556c54fff7a557059841a50410
Ivanka Budimir <ivanka.budimir@petrol.hr>
2023-10-18 13:20:37 +0200

    zadnji ispravak

--- src/job_model/job_model.controller.ts
+++ src/job_model/job_model.controller.ts
@@ -33,11 +33,16 @@ export class JobModelController {
     return job;
   }
 
+ 
   @Get('name/:name')
   getJobsByName(@Param('name') name: string): JobModel[] {
+    if (!name) {
+      throw new NotFoundException('Naziv posla je obavezan.');
+    }
+  
     return this.jobModelService.getJobModel().filter((job) => job.name === name);
   }
-
+  
   @Put(':id')
   updateJob(@Param('id') id: string, @Body() body: JobModelDto): JobModel {
     const job = this.jobModelService.updateJobModel(id, body.name, body.description, body.validTo, body.active);
@@ -48,16 +53,22 @@ export class JobModelController {
   }
 
   @Delete(':id')
-  deleteJobModel(JobModelId: string): boolean {
-    const [, index] = this.findJobModel(JobModelId);
-    if (index !== -1) {
-      this.jobModels.splice(index, 1);
-      return true; 
+deleteJobModel(@Param('id') JobModelId: string): boolean {
+  const [, index] = this.findJobModel(JobModelId);
+  if (index !== -1) {
+    this.jobModels.splice(index, 1);
+    return true;
+  }
+  return false;
+}
+
+  private findJobModel(id: string): [JobModel | undefined, number] {
+    const index = this.jobModels.findIndex((jobModel) => jobModel.id === id);
+    if (index === -1) {
+      return [undefined, -1];
     }
-    return false; 
+    return [this.jobModels[index], index];
   }
-  findJobModel(JobModelId: string): [any, any] {
-    throw new Error('Method not implemented.');
-  }
+  
   
 }
