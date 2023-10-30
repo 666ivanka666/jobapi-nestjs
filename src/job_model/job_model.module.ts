cfbe6f95508e7d31b6f3d34ed576dec5e4c7b376
666ivanka666 <xivankax@gmail.com>
2023-10-30 09:59:32 +0100

    zadnja promjena

--- src/job_model/job_model.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { JobModelController } from './job_model.controller';
-import { JobModelService } from './job_model.service';
-
-@Module({
-  controllers: [JobModelController],
-  providers: [JobModelService]
-})
-export class JobModelModule {}
--- src/job_model/type/index.ts
+++
@@ -1,9 +0,0 @@
-export class JobModel {
-  constructor(
-    public id: string,
-    public name: string,
-    public description: string,
-    public validTo:number, 
-    public active: boolean
-  ) {}
-}
